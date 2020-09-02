a7760f5ee9934d022021216d67357e1184fb6071
Doctr (Travis CI) <drdoctr@users.noreply.github.com>
2020-09-02 01:32:34 +0000

    Update docs after building Travis build 4 of morgatron/spylind
    The docs were built from the branch 'master' against the commit
    857c3363147db662f6747a508562db6e1ed00ff4.
    
    The Travis build that generated this commit is at
    https://travis-ci.org/morgatron/spylind/jobs/723294205.
    
    The doctr command that was run is
    
        /home/travis/virtualenv/python3.6.7/bin/doctr deploy --built-docs docs/build/html .

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.post6+ge359cad',
+    VERSION: '0.post7+g857c336',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
